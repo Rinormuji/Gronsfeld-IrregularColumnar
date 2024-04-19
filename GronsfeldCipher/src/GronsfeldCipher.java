d43540a68391b78d06625d62ad2ae3e809538848
Rita <155006858+ritaademi@users.noreply.github.com>
2024-04-19 23:59:14 +0200

    Update GronsfeldCipher.java

--- GronsfeldCipher/src/GronsfeldCipher.java
+++ GronsfeldCipher/src/GronsfeldCipher.java
@@ -9,6 +9,35 @@ public class GronsfeldCipher {
         this.alphabet = alphabet;
         this.key = key;
     }
+
+    // Encryption part
+    private char encryptChar(char originalChar, char keyChar) {
+        if (!Character.isLetter(originalChar)) {
+            return originalChar;
+        }
+
+        int alphabetIndex = alphabet.indexOf(Character.toUpperCase(originalChar));
+        int keyOffset = keyChar - '0';
+
+        int encryptedIndex = (alphabetIndex + keyOffset) % alphabet.length();
+        char encryptedChar = alphabet.charAt(encryptedIndex);
+
+        return Character.isLowerCase(originalChar) ? Character.toLowerCase(encryptedChar) : encryptedChar;
+    }
+
+    public String encrypt(String plaintext) {
+        StringBuilder ciphertext = new StringBuilder();
+        String repeatedKey = repeatKeyToMatchLength(plaintext);
+
+        for (int i = 0; i < plaintext.length(); i++) {
+            char originalChar = plaintext.charAt(i);
+            char encryptedChar = encryptChar(originalChar, repeatedKey.charAt(i));
+            ciphertext.append(encryptedChar);
+        }
+
+        return ciphertext.toString();
+    }
+        
     public static void main(String[] args) {
         String alphabet = "abcdefghijklmnopqrstuvwxyz";
         String key = "31415";
